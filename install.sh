fe9be7658f98ce790b75892b4553dc8c24999a4c
puzl <harry.walsh@gmail.com>
2014-08-27 11:21:11 +0100

    Changes

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -sf $HOME/.rcm/dotvim $HOME/.vim
+ln -sf $HOME/.rcm/dotvim/vimrc $HOME/.vimrc
+ln -sf $HOME/.rcm/prezto $HOME/.zprezto
+ln -sf $HOME/.rcm/prezto/runcoms/zlogin $HOME/.zlogin
+ln -sf $HOME/.rcm/prezto/runcoms/zlogout $HOME/.zlogout
+ln -sf $HOME/.rcm/prezto/runcoms/zpreztorc $HOME/.zpreztorc
+ln -sf $HOME/.rcm/prezto/runcoms/zprofile $HOME/.zprofile
+ln -sf $HOME/.rcm/prezto/runcoms/zshenv $HOME/.zshenv
+ln -sf $HOME/.rcm/prezto/runcoms/zshrc $HOME/.zshrc
