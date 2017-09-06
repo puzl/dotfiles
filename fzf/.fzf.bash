96f0797deeeb50a0238efe093673613938aebb21
Harry Walsh <harry.walsh@gmail.com>
2017-09-06 13:38:17 +0100

    cleanup some mesh and re-enable fzf

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
-  export PATH="$PATH:$HOME/.fzf/bin"
+if [[ ! "$PATH" == */home/hjw/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/hjw/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/hjw/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.bash"
+source "/home/hjw/.fzf/shell/key-bindings.bash"
 
--- fzf/.fzf.zsh
+++ fzf/.fzf.zsh
@@ -1,15 +1,14 @@
 # Setup fzf
 # ---------
-
-if [[ ! "$PATH" == *$HOME/fzf/bin* ]]; then
-  export PATH="$PATH:$HOME/.fzf/bin"
+if [[ ! "$PATH" == */home/hjw/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/hjw/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/home/hjw/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "$HOME/.fzf/shell/key-bindings.zsh"
+source "/home/hjw/.fzf/shell/key-bindings.zsh"
 
