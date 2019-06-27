c773b8a17834980fc706d0d2f9df8bdb8ee08f05
Harry Walsh <harry.walsh@gmail.com>
2019-06-27 17:03:47 +0100

    updates

--- doom.d/init.el
+++ doom.d/init.el
@@ -185,9 +185,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8aca557e9a17174d8f847fb02870cb2bb67f3b6e808e46c0e54a44e3e18e1020" "d1b4990bd599f5e2186c3f75769a2c5334063e9e541e37514942c27975700370" "9954ed41d89d2dcf601c8e7499b6bb2778180bfcaeb7cdfc648078b8e05348c6" "fd944f09d4d0c4d4a3c82bd7b3360f17e3ada8adf29f28199d09308ba01cc092" "6d589ac0e52375d311afaa745205abb6ccb3b21f6ba037104d71111e7e76a3fc" default))))
+ '(package-selected-packages (quote (async quelpa use-package persistent-soft))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs
+++ emacs
@@ -1,6 +1,18 @@
 (setq use-spacemacs nil)
 (setq use-doom t)
-(cond (use-spacemacs (setq user-emacs-directory "/opt/hjw/emacs/spacemacs/source/"))
-      (use-doom (setq user-emacs-directory "/opt/hjw/emacs/doom/source")))
+
+(if (eq system-type 'gnu/linux)
+  (cond (use-spacemacs (setq user-emacs-directory "/opt/hjw/emacs/spacemacs/source/"))
+        (use-doom (setq user-emacs-directory "/opt/hjw/emacs/doom/source")))
+  )
+
+(if (eq system-type 'cygwin)
+ (setq user-emacs-directory "/home/hjw/.emacs.d")
+  )
+
+(if (eq system-type 'windowsnt)
+ (setq user-emacs-directory "c:/Users/hjwAppData/Roaming/.emacs.d")
+  )
+
 
 (load (expand-file-name "init.el" user-emacs-directory))
