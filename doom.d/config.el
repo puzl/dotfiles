a53b2748531892dea30b9f9660f8a4cb69df1ba0
Harry Walsh <harry.walsh@gmail.com>
2019-07-22 13:40:48 +0100

    Tweaked doom config

--- doom.d/config.el
+++ doom.d/config.el
@@ -55,3 +55,28 @@
 
 (setq flycheck-disabled-checkers '(c/c++-clang c/c++-gcc))
 (setq flycheck-cppcheck-checks "all")
+
+;;-------------
+;;Add color to the current GUD line (obrigado google)
+
+(defvar gud-overlay
+(let* ((ov (make-overlay (point-min) (point-min))))
+(overlay-put ov 'face 'secondary-selection)
+ov)
+"Overlay variable for GUD highlighting.")
+
+(defadvice gud-display-line (after my-gud-highlight act)
+"Highlight current line."
+(let* ((ov gud-overlay)
+(bf (gud-find-file true-file)))
+(save-excursion
+  (set-buffer bf)
+  (move-overlay ov (line-beginning-position) (line-end-position)
+  (current-buffer)))))
+
+(defun gud-kill-buffer ()
+(if (eq major-mode 'gud-mode)
+(delete-overlay gud-overlay)))
+
+(add-hook 'kill-buffer-hook 'gud-kill-buffer)
+;;-------------------------------------------------------------
